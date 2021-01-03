43869ae5f62ae62579a999187ce656f3948bef1b
Arkan M. Gerges <arkan.m.gerges@gmail.com>
2021-01-04 00:11:53 +0200

    Fix request on multiple results

--- src/port_adapter/api/rest/router/v1/identity/request.py
+++ src/port_adapter/api/rest/router/v1/identity/request.py
@@ -40,19 +40,19 @@ async def isRequestSuccessful(*,
         else:
             cacheType = split[0]
             if CacheType.valueToEnum(cacheType) == CacheType.LIST:
-                key = split[1]
-                successRequired = int(split[2])
-                if cacheClient.llen(cacheKey) == successRequired:
-                    items = cacheClient.lrange(cacheKey, 0, -1)
-                    if len(items) < successRequired:
-                        raise InProgressException(f'Request id: {request_id} is still in progress')
-                    for item in items:
-                        result = json.loads(item.decode('utf-8'))
-                        if not result['success']:
-                            return BoolRequestResponse(success=False)
-                    return BoolRequestResponse(success=True)
+                items = cacheClient.lrange(cacheKey, 0, -1)
+                if _hasAtLeastOneFailed(items):
+                    return BoolRequestResponse(success=False)
                 else:
-                    raise InProgressException(f'Request id: {request_id} is still in progress')
+                    _key = split[1]
+                    successRequired = int(split[2])
+                    if cacheClient.llen(cacheKey) == successRequired:
+                        items = cacheClient.lrange(cacheKey, 0, -1)
+                        if len(items) < successRequired:
+                            raise InProgressException(f'Request id: {request_id} is still in progress')
+                        return BoolRequestResponse(success=True)
+                    else:
+                        raise InProgressException(f'Request id: {request_id} is still in progress')
             else:
                 raise UnknownCacheTypeException(f'Request id: {request_id} has unknown cache type {cacheType}')
     except NotFoundException as e:
@@ -80,20 +80,19 @@ async def getRequestIdResult(*, request_id: str = Query(..., description='Reques
         else:
             cacheType = split[0]
             if CacheType.valueToEnum(cacheType) == CacheType.LIST:
-                result = {'items': [], 'items_count': 0}
-                key = split[1]
-                successRequired = int(split[2])
-                if cacheClient.llen(cacheKey) == successRequired:
-                    items = cacheClient.lrange(cacheKey, 0, -1)
-                    if len(items) < successRequired:
-                        raise InProgressException(f'Request id: {request_id} is still in progress')
-                    for item in items:
-                        resultDict = json.loads(item.decode('utf-8'))
-                        result['items'].append({**resultDict['data'], 'creator_service_name': resultDict['creator_service_name']})
-                        result['items_count'] += 1
-                    return ResultRequestResponse(result=result)
+                items = cacheClient.lrange(cacheKey, 0, -1)
+                if _hasAtLeastOneFailed(items):
+                    return ResultRequestResponse(result=_resultFromItems(items))
                 else:
-                    raise InProgressException(f'Request id: {request_id} is still in progress')
+                    _key = split[1]
+                    successRequired = int(split[2])
+                    if cacheClient.llen(cacheKey) == successRequired:
+                        items = cacheClient.lrange(cacheKey, 0, -1)
+                        if len(items) < successRequired:
+                            raise InProgressException(f'Request id: {request_id} is still in progress')
+                        return ResultRequestResponse(result=_resultFromItems(items))
+                    else:
+                        raise InProgressException(f'Request id: {request_id} is still in progress')
             else:
                 raise UnknownCacheTypeException(f'Request id: {request_id} has unknown cache type {cacheType}')
     except NotFoundException as e:
@@ -101,3 +100,20 @@ async def getRequestIdResult(*, request_id: str = Query(..., description='Reques
     except Exception as e:
         logger.info(e)
         return JSONResponse(status_code=400, content={"detail": [{"msg": str(e)}]})
+
+
+def _hasAtLeastOneFailed(items):
+    for item in items:
+        resultDict = json.loads(item.decode('utf-8'))
+        if resultDict['success'] is False:
+            return True
+    return False
+
+
+def _resultFromItems(items):
+    result = {'items': [], 'items_count': 0}
+    for item in items:
+        resultDict = json.loads(item.decode('utf-8'))
+        result['items'].append({**resultDict['data'], 'creator_service_name': resultDict['creator_service_name']})
+        result['items_count'] += 1
+    return result
