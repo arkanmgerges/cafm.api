916bf84f80d4391683c130688ff315d70df854f2
Mohammad Mohammadi <mmdii@develoop.run>
2021-01-06 12:31:51 +0330

    country byID added

--- src/port_adapter/api/rest/grpc/v1/identity/country/CountryClient.py
+++ src/port_adapter/api/rest/grpc/v1/identity/country/CountryClient.py
@@ -2,16 +2,18 @@
 @author: Mohammad S. moso<moso@develoop.run>
 """
 import os
-from typing import List
-
 import grpc
+import src.port_adapter.AppDi as AppDi
+
+from typing import List
+from src.resource.logging.opentelemetry.OpenTelemetry import OpenTelemetry
 
 from src.port_adapter.api.rest.grpc.Client import Client
-from src.port_adapter.api.rest.model.response.v1.identity.Countries import Countries
+from src.port_adapter.api.rest.model.response.v1.identity.Countries import Countries, CountryDescriptor
 from src.port_adapter.api.rest.model.response.v1.identity.Country import Country
 from src.resource.logging.logger import logger
 from src.resource.proto._generated.identity.country_app_service_pb2 import CountryAppService_countriesRequest, \
-    CountryAppService_countriesResponse
+    CountryAppService_countriesResponse, CountryAppService_countryByIdRequest, CountryAppService_countryByIdResponse
 from src.resource.proto._generated.identity.country_app_service_pb2_grpc import CountryAppServiceStub
 
 
@@ -20,6 +22,7 @@ class CountryClient(Client):
         self._server = os.getenv('CAFM_IDENTITY_GRPC_SERVER_SERVICE', '')
         self._port = os.getenv('CAFM_IDENTITY_GRPC_SERVER_SERVICE_PORT', '')
 
+    @OpenTelemetry.grpcTraceOTel
     def countries(self, resultFrom: int = 0, resultSize: int = 10, order: List[dict] = None) -> Countries:
         order = [] if order is None else order
         with grpc.insecure_channel(f'{self._server}:{self._port}') as channel:
@@ -47,6 +50,34 @@ class CountryClient(Client):
                                for country in
                                response[0].countries],
                     item_count=response[0].itemCount)
+            except Exception as e:
+                channel.unsubscribe(lambda ch: ch.close())
+                raise e
+
+    @OpenTelemetry.grpcTraceOTel
+    def countryById(self, countryId) -> CountryDescriptor:
+
+        with grpc.insecure_channel(f'{self._server}:{self._port}') as channel:
+            stub = CountryAppServiceStub(channel)
+            try:
+                logger.debug(
+                    f'[{CountryClient.countryById.__qualname__}] - grpc call to retrieve country with countryId: {countryId} from server {self._server}:{self._port}')
+                response: CountryAppService_countryByIdResponse = stub.countryById.with_call(
+                    CountryAppService_countryByIdRequest(id=countryId),
+                    metadata=(('token', self.token), (
+                        'opentel',
+                        AppDi.instance.get(OpenTelemetry).serializedContext(CountryClient.countryById.__qualname__))))
+                logger.debug(
+                    f'[{CountryClient.countryById.__qualname__}] - grpc response: {response}')
+
+                return CountryDescriptor(id=response[0].country.id,
+                                         geo_name_id=response[0].country.geoNameId,
+                                         locale_code=response[0].country.localeCode,
+                                         continent_code=response[0].country.continentCode,
+                                         continent_name=response[0].country.continentName,
+                                         country_iso_code=response[0].country.countryIsoCode,
+                                         country_name=response[0].country.countryName,
+                                         is_in_european_union=response[0].country.isInEuropeanUnion)
             except Exception as e:
                 channel.unsubscribe(lambda ch: ch.close())
                 raise e
--- src/port_adapter/api/rest/router/v1/identity/country.py
+++ src/port_adapter/api/rest/router/v1/identity/country.py
@@ -5,13 +5,14 @@
 import grpc
 from fastapi import APIRouter, Depends, Query
 from fastapi import Response
+from fastapi.params import Path
 from grpc.beta.interfaces import StatusCode
 from starlette.status import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR, HTTP_403_FORBIDDEN
 
 import src.port_adapter.AppDi as AppDi
 from src.domain_model.OrderService import OrderService
 from src.port_adapter.api.rest.grpc.v1.identity.country.CountryClient import CountryClient
-from src.port_adapter.api.rest.model.response.v1.identity.Countries import Countries
+from src.port_adapter.api.rest.model.response.v1.identity.Countries import Countries, CountryDescriptor
 from src.port_adapter.api.rest.router.v1.identity.auth import CustomHttpBearer
 from src.resource.logging.logger import logger
 
@@ -41,24 +42,23 @@ async def getCountries(*,
     except Exception as e:
         logger.info(e)
 
-# @router.get(path="/{country_id}/", summary='Get country',
-#             response_model=CountryDescriptor)
-# async def getCountry(*, country_id: str = Path(...,
-#                                          description='Country id that is used to fetch country data'),
-#                   _=Depends(CustomHttpBearer())):
-#     """Get a User by id
-#     """
-#     try:
-#         client = UserClient()
-#         return client.userById(userId=user_id)
-#     except grpc.RpcError as e:
-#         if e.code() == StatusCode.PERMISSION_DENIED:
-#             return Response(content=str(e), status_code=HTTP_403_FORBIDDEN)
-#         if e.code() == StatusCode.NOT_FOUND:
-#             return Response(content=str(e), status_code=HTTP_404_NOT_FOUND)
-#         else:
-#             logger.error(
-#                 f'[{getUser.__module__}.{getUser.__qualname__}] - error response e: {e}')
-#             return Response(content=str(e), status_code=HTTP_500_INTERNAL_SERVER_ERROR)
-#     except Exception as e:
-#         logger.info(e)
+
+@router.get(path="/{country_id}/", summary='Get country', response_model=CountryDescriptor)
+async def getCountry(*, country_id: str = Path(..., description='Country id that is used to fetch country data'),
+                     _=Depends(CustomHttpBearer())):
+    """Get a Country by id
+    """
+    try:
+        client = CountryClient()
+        return client.countryById(countryId=country_id)
+    except grpc.RpcError as e:
+        if e.code() == StatusCode.PERMISSION_DENIED:
+            return Response(content=str(e), status_code=HTTP_403_FORBIDDEN)
+        if e.code() == StatusCode.NOT_FOUND:
+            return Response(content=str(e), status_code=HTTP_404_NOT_FOUND)
+        else:
+            logger.error(
+                f'[{getCountry.__module__}.{getCountry.__qualname__}] - error response e: {e}')
+            return Response(content=str(e), status_code=HTTP_500_INTERNAL_SERVER_ERROR)
+    except Exception as e:
+        logger.info(e)
