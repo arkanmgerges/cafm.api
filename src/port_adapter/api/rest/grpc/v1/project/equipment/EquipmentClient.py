affc80bfef2f35dfd79ad42108d9cc494c0c69c0
Arkan M. Gerges <arkan.m.gerges@gmail.com>
2021-03-18 23:29:24 +0200

    Fix create equipment

--- src/port_adapter/api/rest/grpc/v1/project/equipment/EquipmentClient.py
+++ src/port_adapter/api/rest/grpc/v1/project/equipment/EquipmentClient.py
@@ -19,7 +19,7 @@ from src.resource.logging.opentelemetry.OpenTelemetry import OpenTelemetry
 from src.resource.proto._generated.project.equipment_app_service_pb2 import \
     EquipmentAppService_equipmentsResponse, \
     EquipmentAppService_equipmentsRequest, EquipmentAppService_equipmentByIdRequest, \
-    EquipmentAppService_equipmentByIdResponse
+    EquipmentAppService_equipmentByIdResponse, EquipmentAppService_newIdRequest, EquipmentAppService_newIdResponse
 from src.resource.proto._generated.project.equipment_app_service_pb2_grpc import EquipmentAppServiceStub
 
 
@@ -27,6 +27,23 @@ class EquipmentClient(Client):
     def __init__(self):
         self._server = os.getenv('CAFM_PROJECT_GRPC_SERVER_SERVICE', '')
         self._port = os.getenv('CAFM_PROJECT_GRPC_SERVER_SERVICE_PORT', '')
+
+    @OpenTelemetry.grpcTraceOTel
+    def newId(self) -> str:
+        with grpc.insecure_channel(f'{self._server}:{self._port}') as channel:
+            stub = EquipmentAppServiceStub(channel)
+            try:
+                request = EquipmentAppService_newIdRequest()
+                response: EquipmentAppService_newIdResponse = stub.newId.with_call(
+                    request,
+                    metadata=(('token', self.token), (
+                        'opentel', AppDi.instance.get(OpenTelemetry).serializedContext(EquipmentClient.newId.__qualname__))))
+                logger.debug(
+                    f'[{EquipmentClient.newId.__qualname__}] - grpc response: {response}')
+                return response[0].id
+            except Exception as e:
+                channel.unsubscribe(lambda ch: ch.close())
+                raise e
 
     @OpenTelemetry.grpcTraceOTel
     def equipments(self, resultFrom: int = 0, resultSize: int = 10, order: List[dict] = None) -> Equipments:
