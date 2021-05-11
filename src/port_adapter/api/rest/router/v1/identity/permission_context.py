b7ce0ec4cde14ccd3faef398986984ad4cc45cb3
Mohammad Mohammadi <mmdii@develoop.run>
2021-05-11 17:07:21 +0430

    delete permission context bug fixed

--- src/port_adapter/api/rest/router/v1/identity/permission_context.py
+++ src/port_adapter/api/rest/router/v1/identity/permission_context.py
@@ -177,7 +177,7 @@ async def deletePermissionContext(
     producer.produce(
         obj=ApiCommand(
             id=reqId,
-            name=CommandConstant.DELETE_RESOURCE_TYPE.value,
+            name=CommandConstant.DELETE_PERMISSION_CONTEXT.value,
             metadata=json.dumps({"token": Client.token}),
             data=json.dumps({"permission_context_id": permission_context_id}),
         ),
