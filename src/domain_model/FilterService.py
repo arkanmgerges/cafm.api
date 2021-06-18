0db010a81a2abc87399737b6e858e0fbbb363f4d
bc <bara.catalin@gmail.com>
2021-06-18 13:00:42 +0300

    add filter to user lookup

--- src/domain_model/FilterService.py
+++ src/domain_model/FilterService.py
@@ -13,6 +13,8 @@ class FilterService:
         if len(filterArray) > 0:
             for item in filterArray:
                 split = [item.strip() for item in item.split(":")]
-                if len(split) > 1:
+                if len(split) == 2:
                     result.append({"key": split[0], "value": split[1]})
+                if len(split) == 3:
+                    result.append({"key": split[0], "operator": split[1], "value": split[2]})
         return result
