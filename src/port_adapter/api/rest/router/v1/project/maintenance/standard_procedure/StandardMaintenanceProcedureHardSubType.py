d6def991cd951413d403675c92d0dba4d962dcb4
Arkan M. Gerges <arkan.m.gerges@gmail.com>
2021-06-25 17:30:48 +0300

    Add standard maintenance procedure/operation/parameter

---
+++ src/port_adapter/api/rest/router/v1/project/maintenance/standard_procedure/StandardMaintenanceProcedureHardSubType.py
@@ -0,0 +1,10 @@
+"""
+@author: Mohammad S. moso<moso@develoop.run>
+"""
+
+from enum import Enum
+
+
+class StandardMaintenanceProcedureHardSubType(str, Enum):
+    OUTSOURCED = "outsourced"
+    IN_HOUSE = "in_house"
